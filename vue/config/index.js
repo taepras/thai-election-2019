ea0aafa8aff0b58d2f24491f8372136556341904
Thanawit Prasongpongchai <taepras@gmail.com>
2019-02-12 23:42:28 -0500

    fix vue dist

--- vue/config/index.js
+++ vue/config/index.js
@@ -43,7 +43,8 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/thai-election-2019/vue/dist/',
+    // assetsPublicPath: '/',
 
     /**
      * Source Maps
